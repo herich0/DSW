6ee5b055e3d936009cd5bca1040a88ea2de0e56c
Herich <herichop013@gmail.com>
2025-04-25 15:10:42 -0300

    commitando alterações 25/04/2025

--- API/src/modules/products/routes/products.routes.ts
+++ API/src/modules/products/routes/products.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { celebrate,Joi,Segments } from "celebrate";
 import ProductsController from "../controllers/ProductsController";
 
 const productsRouter =Router();
@@ -12,36 +13,55 @@ productsRouter.get('/',async(req,res,next)=>{
     }
 });
 
-productsRouter.get('/:id',async(req,res,next)=>{
-    try{
-        await productsController.index(req, res,next);
-    }catch(err){
-        next(err);
+productsRouter.get('/:id', celebrate({
+    [Segments.PARAMS] : {id: Joi.string().uuid().required()}
+  }),
+  async (req, res, next) => {
+    try {
+      await productsController.show(req, res, next);
+    } catch (err) {
+      next(err);
     }
-});
+  });
 
-productsRouter.post('/',async(req,res,next)=>{
-    try{
-        await productsController.create(req,res,next);
-    }catch(err){
-        next(err);
+  productsRouter.post('/',  celebrate({
+    [Segments.BODY]: {
+        name: Joi.string().required(),
+        price: Joi.number().precision(2).required(),
+        quantity: Joi.number().required(),
     }
-});
+  }),
+  async (req, res, next) => {
+    try {
+      await productsController.create(req, res, next);
+    } catch (err) {
+      next(err);
+    }
+  });
 
-productsRouter.put('/',async(req,res,next)=>{
-    try{
-        await productsController.update(req,res,next);
-    }catch(err){
-        next(err);
+  productsRouter.put('/:id',  celebrate({
+    [Segments.PARAMS] : {id: Joi.string().uuid().required()},
+    [Segments.BODY]: {
+        name: Joi.string().required(),
+        price: Joi.number().precision(2).required(),
+        quantity: Joi.number().required(),
     }
-});
+  }),async (req, res, next) => {
+    try {
+      await productsController.update(req, res, next);
+    } catch (err) {
+      next(err);
+    }
+  });
 
-productsRouter.delete('/',async(req,res,next)=>{
-    try{
-        await productsController.delete(req,res,next);
-    }catch(err){
-        next(err);
+  productsRouter.delete('/:id', celebrate({
+    [Segments.PARAMS] : {id: Joi.string().uuid().required()}
+  }), async (req, res, next) => {
+    try {
+      await productsController.delete(req, res, next);
+    } catch (err) {
+      next(err);
     }
-});
+  });
 
 export default productsRouter;
--- API/src/shared/http/server.ts
+++ API/src/shared/http/server.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request,Response } from 'express';
+import {errors} from 'celebrate';
+import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
@@ -9,6 +11,7 @@ const app =express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.use((error:Error, request:Request,response:Response, next:NextFunction) =>{
     if (error instanceof AppError){
